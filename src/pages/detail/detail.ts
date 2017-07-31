0eab6d6e1f8987795822c49e6f597967c342f85c
shun.utsunomiya <noanoamn@gmail.com>
2017-07-31 10:57:36 +0900

    oversmashでのJSONデータ取得サンプルの追加

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import oversmash from 'oversmash';
-import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-detail',
@@ -10,17 +9,24 @@ import { Http } from '@angular/http';
 export class DetailPage {
 
   ow;
-  url;
 
-  constructor(public navCtrl: NavController, http: Http) {
+  constructor(public navCtrl: NavController) {
     this.ow = oversmash();
     this.ow.player('Noanoa-1926').then(player => {
-      console.log(player)
+      console.log(JSON.stringify(player))
     })
-    // this.url = "https://owapi.net/api/v3/u/Noanoa-1926/blob";
-    // http.get(url).success(function (data) {
-    //   console.log(data);
-    // });
+    // サポートヒーローのプレイヤースタッツ
+    this.ow.playerStats('Noanoa-1926', 'kr', 'pc').then(player => {
+      console.log(JSON.stringify(player))
+    })
+    // オフェンスヒーローのプレイヤースタッツ
+    this.ow.playerStats('hoshimi-11424', 'kr', 'pc').then(player => {
+      console.log(JSON.stringify(player))
+    })
+    // タンクヒーローのプレイヤースタッツ
+    this.ow.playerStats('gappo3-1173', 'kr', 'pc').then(player => {
+      console.log(JSON.stringify(player))
+    })
   }
 
 }
