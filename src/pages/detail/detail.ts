f6867ca2fe1e74dc3836cea6c0cf196c35eee4af
eggmobile <eggmobile@gmail.com>
2017-08-09 12:28:05 +0900

    詳細画面のデザインを修正。検索画面と統一。(テンプレートは別）

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { USER_INFO_INITIAL_DATA } from '../../assets/data/user-info-initial-data';
+import { HeroesArrayService } from '../../utilities/heroes-array-service';
 // サンプルデータ
 import { NOANOA_1926 } from '../../assets/sample-data/Noanoa-1926';
 import { HOSHIMI_11424 } from '../../assets/sample-data/hoshimi-11424';
@@ -7,14 +9,19 @@ import { GAPPO3_1173 } from '../../assets/sample-data/gappo3-1173';
 
 @Component({
   selector: 'page-detail',
-  templateUrl: 'detail.html'
+  templateUrl: 'detail.html',
+  providers: [HeroesArrayService]
 })
 export class DetailPage {
 
   userInfo;
   heroesArray;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private heroesArrayService: HeroesArrayService) {
+    this.userInfo = USER_INFO_INITIAL_DATA;
+
+    // サービスを利用して、検索結果からヒーローの配列を成形する
+    this.heroesArray = this.heroesArrayService.getHeroesArray(this.userInfo);
     // this.ow = oversmash();
     // this.ow.player('Noanoa-1926').then(player => {
     //   console.log(JSON.stringify(player))
